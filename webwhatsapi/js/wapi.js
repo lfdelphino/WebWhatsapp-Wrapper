d1a2943e2426a40af2362f65605d82f3a295cbbf
Luiz Delphino <lf.delphino@live.com>
2018-09-18 19:09:00 -0400

    Update wapi.js

--- webwhatsapi/js/wapi.js
+++ webwhatsapi/js/wapi.js
@@ -2,7 +2,6 @@
 /**
  * This script contains WAPI functions that need to be run in the context of the webpage
  */
-
 /**
  * Auto discovery the webpack object references of instances that contains all functions used by the WAPI
  * functions and creates the Store object.
